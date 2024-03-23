835eaa7332ea051573bd90a5958c32a2db0d3fb6
d2ski <chukotski@gmail.com>
2024-03-23 16:25:36 +0200

    feat: add routes titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,20 @@ export const routes: Routes = [
   {
     path: 'list',
     loadComponent: () => import('./tasks-list/tasks-list.component'),
+    title: 'Список задач',
   },
   {
     path: 'task/:taskId',
     loadComponent: () => import('./task/task.component'),
+    title: 'Детали задачи',
   },
   {
     path: '',
     redirectTo: 'list',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'list',
   },
 ];
